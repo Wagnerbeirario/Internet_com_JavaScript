07fa557b3d85686075dc553cf21b80865647ee29
Wagnerbeirario <46856451+Wagnerbeirario@users.noreply.github.com>
2022-05-29 22:21:35 -0300

    Iniciando os trabalhos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+/*var idade =17;
+if(idade =18)
+{
+    alert("maior de idade")
+}
+else
+{
+    ("menor de idade")
+}*/
+
+/*var nome = "Wagner Magalhães";
+var idade = 37;
+alert(nome + " Tem " + idade +" anos");
+*/
